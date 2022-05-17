0bfa5ffde2bf39a63685709f14e35d656309a2ae
Kumar Shivam <98182315+kumarshivam01@users.noreply.github.com>
2022-05-17 23:11:56 +0530

    done

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,32 +1,69 @@
-import react, { useState } from 'react'
+import react, { useState } from "react";
 
-const AddContact = (props)=>{
-  const [First_Name, setFirst_Name] = useState("")
-  const [Last_Name, setLast_Name] = useState("")
-  const [Email, setEmail] = useState("")
-  const [Pic, setPic] = useState("")
-  const [Phone, setPhone] = useState("")
+const AddContact = (props) => {
+  const [First_Name, setFirst_Name] = useState("");
+  const [Last_Name, setLast_Name] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Pic, setPic] = useState("");
+  const [Phone, setPhone] = useState("");
 
-  const handleClick = ()=>{
-    if(props.handleClick) props.handleClick(First_Name,Last_Name,Email,Pic,Phone);
-    setFirst_Name("")
-    setLast_Name("")
-    setEmail("")
-    setPic("")
-    setPhone("")
-  }
-  return(
-    <div style={{padding:'20px', display:"flex", gap:"10px"}}>
+  const handleClick = () => {
+    if (props.handleClick)
+      props.handleClick(First_Name, Last_Name, Email, Pic, Phone);
+    setFirst_Name("");
+    setLast_Name("");
+    setEmail("");
+    setPic("");
+    setPhone("");
+  };
+  return (
+    <div style={{ padding: "20px", display: "flex", gap: "10px" }}>
       <form action="">
-      <input style={{margin:"5px", padding:'3px 10px', outline:'none'}} type="text" required placeholder="Enter Your First Name" value={First_Name} onChange={(e)=> setFirst_Name(e.target.value)}/>
-      <input style={{margin:"5px", padding:'3px 10px', outline:'none'}} type="text" required placeholder="Enter Your Last Name" value={Last_Name} onChange={(e)=> setLast_Name(e.target.value)}/>
-      <input style={{margin:"5px", padding:'3px 10px', outline:'none'}} type="email" placeholder="Enter Your Email" value={Email} onChange={(e)=> setEmail(e.target.value)}/>
-      <input style={{margin:"5px", padding:'3px 10px', outline:'none'}} type="text" required placeholder="Enter Your image Address" value={Pic} onChange={(e)=> setPic(e.target.value)}/>
-      <input style={{margin:"5px", padding:'3px 10px', outline:'none'}} type="Number" required placeholder="Enter Your Phone no" value={Phone} onChange={(e)=> setPhone(e.target.value)}/><br/>
-      <button type="submit" onClick={handleClick}>Submit</button>
+        <input
+          style={{ margin: "5px", padding: "3px 10px", outline: "none" }}
+          type="text"
+          required
+          placeholder="Enter Your First Name"
+          value={First_Name}
+          onChange={(e) => setFirst_Name(e.target.value)}
+        />
+        <input
+          style={{ margin: "5px", padding: "3px 10px", outline: "none" }}
+          type="text"
+          required
+          placeholder="Enter Your Last Name"
+          value={Last_Name}
+          onChange={(e) => setLast_Name(e.target.value)}
+        />
+        <input
+          style={{ margin: "5px", padding: "3px 10px", outline: "none" }}
+          type="email"
+          placeholder="Enter Your Email"
+          value={Email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          style={{ margin: "5px", padding: "3px 10px", outline: "none" }}
+          type="text"
+          required
+          placeholder="Enter Your image Address"
+          value={Pic}
+          onChange={(e) => setPic(e.target.value)}
+        />
+        <input
+          style={{ margin: "5px", padding: "3px 10px", outline: "none" }}
+          type="Number"
+          required
+          placeholder="Enter Your Phone no"
+          value={Phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <button type="submit" onClick={handleClick}>
+          Submit
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddContact
+export default AddContact;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,45 @@
-import react from 'react'
+import react, { useState } from "react";
 
 export default function Contact(props) {
-  
+  const [isActive, setIsActive] = useState(false);
+  // if(isActive){
+  //   return <div onClick={()=> setIsActive(false)}>heloo</div>
+  // }
   return (
     <>
-     {
-       props.List.map((value)=>{
-         return(
-           <div style={{display:"flex", gap:"15px", cursor:'pointer',}}>
-              <div style={{padding:"10px"}}> <img style={{width:'50px',height:'50px', borderRadius:"50%",}} src={value.pic} alt={value.first_Name}/> </div>
-              <div style={{display:'grid', marginTop:'-10px'}}> <span style={{textTransform:"capitalize",display:'flex',gap:'5px'}}><h3>{value.first_Name} </h3> <h3>{value.last_Name} </h3></span> <div style={{marginTop:"-23px"}}><span>{value.email}</span></div></div>
-           </div>
-         )
-       })
-     }
+      {props.List.map((value) => {
+        return (
+          <div
+            onClick={() => setIsActive(!isActive)}
+            style={{ display: "flex", gap: "15px", cursor: "pointer" }}
+          >
+            <div style={{ padding: "10px" }}>
+              {" "}
+              <img
+                style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+                src={value.pic}
+                alt={value.first_Name}
+              />{" "}
+            </div>
+            <div style={{ display: "grid", marginTop: "-10px" }}>
+              {" "}
+              <span
+                style={{
+                  textTransform: "capitalize",
+                  display: "flex",
+                  gap: "5px"
+                }}
+              >
+                <h3>{value.first_Name} </h3> <h3>{value.last_Name} </h3>
+              </span>{" "}
+              <div style={{ marginTop: "-23px" }}>
+                <span>{value.email}</span>
+              </div>
+              {isActive && <div>{value.phone}</div>}
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 }
